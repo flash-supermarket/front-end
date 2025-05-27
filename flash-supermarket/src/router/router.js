81640d3b64708976aee88b797e6c3eb77ecc516e
Shaohan Yu <doublepi628@163.com>
2025-05-27 21:52:17 +0800

    feat: merge success!

--- flash-supermarket/src/router/router.js
+++ flash-supermarket/src/router/router.js
@@ -1,8 +1,5 @@
 import { createWebHistory, createRouter } from 'vue-router'
 
-import HelloWorld from '../components/HelloWorld.vue'
-import TheWelcome from '../components/TheWelcome.vue'
-
 const routes = [
   { path: '/login', component: loginRegister },
   { path: '/', component: home },
